8c01f1708f416b9f1bb2de321b9f743b1ebd4904
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-11-15 21:02:30 +0900

    テストゲーム作成

---
+++ Assets/AsteroidGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    public GameObject m_asteroidPrefab;
+    public Transform[] m_spawnPositions;
+    public float m_initialInterval = 1.0f;
+    public float m_minimumInterval = 0.2f;
+    public float m_IntervalStep = 0.03f;
+    float m_timer;
+    float m_interval;
+
+	void Start ()
+    {
+        m_interval = m_initialInterval;
+	}
+	
+	void Update ()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer < m_interval) return;
+
+        m_timer = 0f;
+        int i = Random.Range(0, m_spawnPositions.Length);
+        Instantiate(m_asteroidPrefab, m_spawnPositions[i].position, m_asteroidPrefab.transform.rotation);
+        m_interval = Mathf.Max(m_minimumInterval, m_interval - m_IntervalStep);    
+	}
+}
---
+++ Assets/BirdGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdGenerator : MonoBehaviour {
+    public GameObject m_birdPrefab;
+    public Transform[] m_spawnPositions;
+    public float m_interval = 1f;
+    float m_timer;
+    float m_birdPower = 1.0f;
+
+	void Update ()
+    {
+        m_timer += Time.deltaTime;
+
+        if (m_timer < m_interval) return;
+
+        m_timer = 0;
+        int i = Random.Range(0, m_spawnPositions.Length);
+        GameObject go = Instantiate(m_birdPrefab, m_spawnPositions[i].position, m_birdPrefab.transform.rotation);
+        float p = Random.Range(0.5f, 2f);
+        go.GetComponent<BirdController>().Power = m_birdPower;
+        m_birdPower += 0.1f;
+	}
+}
